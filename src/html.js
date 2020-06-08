eebcfdcfeba683cecc2c604408d42db331905d97
suzie <suuuzie18@gmail.com>
2020-06-08 13:38:26 +0900

    Chore

--- src/html.js
+++ src/html.js
@@ -8,7 +8,7 @@ export default function HTML(props) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168813339-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'UA-168813339-1');
